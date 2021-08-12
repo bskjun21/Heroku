57faee70c87a4d1bff8e3f768b18a825b100b087
bskjun21 <18041074@ccmailg.meijo-u.ac.jp>
2021-08-13 00:56:18 +0900

    NEW

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+//  Compooserでインストールしたライブラリを一括読み込み
+require_once __DIR__ . '/vendor/autoload.php';
+
+//  アクセストークンを使いCurlHTTPClientをインスタンス化
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv
+                                                         ('CHANNEL_ACCESS_TOKEN'));
+//  CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+//  LINE Messaging APIがリクエストに付与した署名を取得
+$signature = $_SERVER['HTTP_' . \LINE\LINBot\Constant\HTTPHeader::LINE_SIGNATURE];
+//  署名が正当かチェック。政党であればリクエストをパース配列へ
+$event = $bot->paeseEventRequest(file_get_contents('php://input'), $signature);
+
+//  配列に格納された各イベントをループで処理
+foreach ($event as $event){
+  //  テキストを返信
+  $bot->replyText($event->getReplyToken(), 'かれん');
+}
+?>
