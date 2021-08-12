5c5324e687eeee3d7e5624dd06f6cdb01caed701
bskjun21 <39161860+bskjun21@users.noreply.github.com>
2021-08-13 00:34:55 +0900

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-//  Compooserでインストールしたライブラリを一括読み込み
-require_once __DIR__ . '/vendor/autoload.php';
-
-//  POSTメソッドで渡される値を取得、表示
-$inputString = file_get_contents('php://input');
-error_log($inputString)
-?>
