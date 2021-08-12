9fc138dac79a38d67e77f3f84454419b0de39848
bskjun21 <18041074@ccmailg.meijo-u.ac.jp>
2021-08-12 23:33:15 +0900

    new file

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-echo "Hello World!"
+//  Compooserでインストールしたライブラリを一括読み込み
+require_once__DIR__ . '/vendor/autoload.php';
+
+//  POSTメソッドで渡される値を取得、表示
+$inputString = file_get_contents('php://input');
+error_log($inputString)
 ?>
