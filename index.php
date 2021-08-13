9b2ce4bda46903d352ffa35a3faed7559225ddc0
bskjun21 <18041074@ccmailg.meijo-u.ac.jp>
2021-08-13 11:46:23 +0900

    ''

--- index.php
+++ index.php
@@ -14,8 +14,12 @@ $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
 //  配列に格納された各イベントをループで処理
 foreach ($events as $event){
   //  テキストを返信
-  //  $bot->replyText($event->getReplyToken(), 'かれん');
-  replyTextMessage($bot, $event->getReplyToken(), 'かれん');
+  //replyTextMessage($bot, $event->getReplyToken(), 'かれん');
+  replyImageMessage($bot, $event->replyToken(), 'https://' .
+                    $SERVER['HTTP_HOST'] .
+                    '/imgs/original.jpg', 
+                    'https://' . $SERVER['HTTP_HOST'] .
+                    '/imgs/preview.jpg'); 
 }
 
 //  テキストを返信。引数はLINEBot、返信先、テキスト
@@ -27,6 +31,15 @@ function replyTextMessage($bot, $replyToken, $text){
   //  レスポンスが異常な場合
   if (!$response->isSucceeded()){
     //  エラー内容の出力
+    error_log('Failed! '. $response->getHTTPStatus . ' ' . $response->getRawBody());
+  }
+}
+
+//  画像を返信。引数はLINEBot、返信先、画像URL、サムネイルURL
+function replyImageMessage($bot, $replyToken, $originalImageUrl, $previewImageUrl){
+  //  ImageMessageBuilderの引数は画像URL、サムネイルURL
+  $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder($originalImageUrl, $previewImageUrl));
+  if (!$response->isSucceeded()){
     error_log('Failed! '. $response->getHTTPStatus . ' ' . $response->getRawBody());
   }
 }
